e873f8ab9d8274284a282cabc281bcca1982e2b9
Vesa Siivola <vesa.s.siivola@gmail.com>
2014-03-14 19:34:17 +0200

    localize registration pages

--- vesamusictraining/static/js/simple_translator.js
+++ vesamusictraining/static/js/simple_translator.js
@@ -46,28 +46,86 @@ function get_transtable(pagename, language) {
         return null;
     }
 
+    if (pagename=="loginpage") {
+        if (language=="fi") {
+            ttable["Username"] = "Käyttäjätunnus";
+            ttable["Sign in"] = "Kirjaudu sisään";
+            ttable["Sorry, that's not a valid username or password"] = "Väärä tunnus tai salasana";
+            ttable["Password"] = "Salasana";
+            return ttable;
+        }
+        return null;
+    }
+
+    if (pagename=="regformpage") {
+        if (language=="fi") {
+            ttable["Register"] = "Luo käyttäjätunnus";
+            ttable["Username:"] = "Käyttäjätunnus";
+            ttable["E-mail:"] = "Sähköpostiosoite:";
+            ttable["Password:"] = "Salasana:";
+            ttable["Password (again):"] = "Salasana (uudestaan):";
+            ttable["Submit"] = "Lähetä";
+            ttable["Enter a valid email address."] = "Anna toimiva sähköpostiosoite";
+            return ttable;
+        }
+        return null;
+    }
 }
 
 function translate_page(pagename, language) {
     var transtable = get_transtable(pagename, language);
 
+    real_translate_page(transtable);
+}
+
+function real_translate_page(transtable) {
     if (transtable) {
         $(".localizestring").each(function() {
             $( this ).html(transtable[$(this).text()]);
+        });
+        $("input.ui-button").each(function() {
+            var t = transtable[$(this).val()]
+            if (t) {
+                $(this).val(t);
+            }
         });
     }
 }
 
 function Translator(pagename, language) {
     this.transtable = get_transtable(pagename, language);
-    
+    var transtable = this.transtable;
+
+    this.translate_page = function() { real_translate_page(this.transtable); }
+
     if (this.transtable) {
         this.tp = function(phrase) {
             return this.transtable[phrase];
+        }
+
+        this.translate_form = function() {
+            $("label").each(function() {
+                $( this ).html(transtable[$(this).text()]);
+            });
+
+            $("input").each(function() {
+                var t = transtable[$(this).val()]
+                if (t) {
+                    $(this).val(t);
+                }
+            });
+
+            $("li").each(function() {
+                var t = transtable[$(this).text()]
+                if (t) {
+                    $(this).html(t);
+                }
+            });
         }
     } else {
         this.tp = function(phrase) {
             return phrase;
         }
+        this.translate_form = function() { }
     }
 }
