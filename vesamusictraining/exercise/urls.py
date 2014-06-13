dcde00f670b4908123e0525eda0ffbad1a75f7da
Vesa Siivola <vesa.siivola@lingsoft.fi>
2014-06-13 22:59:55 +0300

    the chooser screen is now rendered with django templates

--- vesamusictraining/exercise/urls.py
+++ vesamusictraining/exercise/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url, include
 urlpatterns = patterns('vesamusictraining.exercise.views',
                        (r'^$', "exercise"),
                        url(r'^list_lectures/$', "list_lectures"),
+                       url(r'^show_lectures/$', "show_lectures"),
                        url(r'^(?P<lecture_name>.*?)/lecture/', "lecture"),
                        url(r'^(?P<lecture_name>.*?)/verify/', "verify"),
                        url(r'^(?P<lecture_name>.*?)/complete/', "register_completion")
